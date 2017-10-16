ab85ec41c81701ab28550c69648972630cf6f781
Colter McQuay <colter.mcquay@gmail.com>
2017-10-16 15:11:06 -0700

    Adding flexlibmono as submodule - updated build scripts to call flexlibmonos build.sh - removing dll's from repo since they are now built from submodule

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
 pushd `dirname $0` > /dev/null
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 popd > /dev/null
 BUILDDIR="${SCRIPTPATH}/build"
+GRFLEXLIBDIR="${SCRIPTPATH}/python/FlexlibMono"
+FLEXLIBMONODIR="${SCRIPTPATH}/FlexlibMono"
+FLEXLIBOUTPUTDIR="${FLEXLIBMONODIR}/tools"
+FLEXLIBMONOBUILD="${FLEXLIBMONODIR}/buildtools.sh"
+
+echo "Building FlexLibMono ${FLEXLIBMONOBUILD}"
+/bin/bash "${FLEXLIBMONOBUILD}"
+
+if [ ! -d "$GRFLEXLIBDIR" ]; then
+    mkdir "$GRFLEXLIBDIR"
+fi
+
+echo "Copying FlexLibMono to gr-flex"
+cp $FLEXLIBOUTPUTDIR/*.dll $GRFLEXLIBDIR
 
 if [ ! -d "$BUILDDIR" ]; then
     mkdir "$BUILDDIR"

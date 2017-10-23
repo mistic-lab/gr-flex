6095d49b55d3555d72dbce4ada89f063594b4b85
Colter McQuay <colter.mcquay@gmail.com>
2017-10-23 11:04:57 -0700

    Cleaned up pylint in ring buffer

--- python/RingBuffer.py
+++ python/RingBuffer.py
@@ -1,14 +1,26 @@
-import numpy as np
+"""
+A 1D ring buffer using numpy arrays
+"""
 from threading import RLock
+import numpy as np
 
-class RingBuffer():
+class RingBuffer(object):
+    """
+    A 1D ring buffer using numpy arrays
+    """
+
     class Empty(Exception):
+        """
+        The RingBuffer.Empty Exception thrown when a read is issued on an empty buffer
+        """
         pass
-    
-    class Full(Exception):
-        pass  
 
-    "A 1D ring buffer using numpy arrays"
+    class Full(Exception):
+        """
+        The RingBuffer.Full Exception thrown when a write is issued on a full buffer
+        """
+        pass
+
     def __init__(self, length=0):
         self.data = np.zeros(length, dtype='f')
         self.__size_used = 0
@@ -31,41 +43,42 @@ class RingBuffer():
     def size_used(self):
         """ Gets the current amount of the buffer that is used but unread """
         with self.__lock:
-            return self.__size_used        
+            return self.__size_used
 
     @property
     def is_empty(self):
+        """ Gets whether or not thxe buffer is currently empty """
         with self.__lock:
             return self.__size_used == 0
 
-    def add(self, x):
-        """adds data x to ring buffer"""
-        x_size = 0
-        if(type(x)is list):
-            x_size = len(x)
-        elif(type(x) is np.ndarray):
-            x_size = x.size
+    def add(self, items):
+        """adds data items to ring buffer"""
+        items_size = 0
+        if isinstance(items, list):
+            items_size = len(items)
+        elif isinstance(items, np.ndarray):
+            items_size = items.size
 
         with self.__lock:
-            if(self.size_remaining == 0):
-                raise RingBuffer.Full      
+            if self.size_remaining == 0:
+                raise RingBuffer.Full
 
-            x_index = (self.index + np.arange(x_size)) % self.data.size
-            self.data[x_index] = x
-            self.index = x_index[-1] + 1
-            self.__size_used += x_size
+            items_index = (self.index + np.arange(items_size)) % self.data.size
+            self.data[items_index] = items
+            self.index = items_index[-1] + 1
+            self.__size_used += items_size
 
-    def get(self,number):
+    def get(self, number):
         """Returns the first-in-first-out data in the ring buffer"""
         with self.__lock:
-            if(self.is_empty):
+            if self.is_empty:
                 raise RingBuffer.Empty
 
             # if someone requests more data than in the buffer, just
             # give them the rest of the buffer
-            number= min(number,self.size_used)
+            number = min(number, self.size_used)
 
-            out = np.roll(self.data,self.read_index)[0:number]
+            out = np.roll(self.data, self.read_index)[0:number]
             self.__size_used -= number
             self.read_index += number
             return out

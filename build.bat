2b72a8b24ce9d92415975eea3619c8be96c6c70e
Unknown <saltair93+GitHub@gmail.com>
2017-12-17 16:42:12 -0800

    Made some edits. I guess you could say things are getting pretty serious

--- build.bat
+++ build.bat
@@ -1,28 +1,26 @@
 :: batch
-pushd `dirname $0` > /dev/null
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-popd > /dev/null
-BUILDDIR="${SCRIPTPATH}/build"
-GRFLEXLIBDIR="${SCRIPTPATH}/python/FlexlibMono"
-FLEXLIBMONODIR="${SCRIPTPATH}/FlexlibMono"
-FLEXLIBOUTPUTDIR="${FLEXLIBMONODIR}/tools"
-FLEXLIBMONOBUILD="${FLEXLIBMONODIR}/buildtools.sh"
+:: Do we need this if we tell them what folder to execute the script from?
+pushd %~dp0 1> NUL
+set SCRIPTPATH=%CD%
+popd 1> NUL
 
-echo "Building FlexLibMono ${FLEXLIBMONOBUILD}"
-/bin/bash "${FLEXLIBMONOBUILD}"
+set BUILDDIR=%SCRIPTPATH%\build
+set GRFLEXLIBDIR=%SCRIPTPATH%\python\FlexlibMono
+set FLEXLIBMONODIR=%SCRIPTPATH%\FlexlibMono
+set FLEXLIBOUTPUTDIR=%FLEXLIBMONODIR%\tools
+set FLEXLIBMONOBUILD=%FLEXLIBMONODIR%\buildtools.bat
 
-if [ ! -d "$GRFLEXLIBDIR" ]; then
-    mkdir "$GRFLEXLIBDIR"
-fi
+echo "Building FlexLibMono %FLEXLIBMONOBUILD%"
+call %FLEXLIBMONOBUILD%
+
+if not exist %GRFLEXLIBDIR% mkdir %GRFLEXLIBDIR%
 
 echo "Copying FlexLibMono to gr-flex"
-cp $FLEXLIBOUTPUTDIR/*.dll $GRFLEXLIBDIR
+copy %FLEXLIBOUTPUTDIR%/*.dll %GRFLEXLIBDIR%
 
-if [ ! -d "$BUILDDIR" ]; then
-    mkdir "$BUILDDIR"
-fi
+if not exist %BUILDDIR%  mkdir %BUILDDIR%
 
-cd "$BUILDDIR"
+cd %BUILDDIR%
 
 cmake ../
 make
